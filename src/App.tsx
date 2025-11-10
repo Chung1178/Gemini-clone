a3c0d12057fccb4343b3a3b737739c646713f2f1
Chung1178 <allen117805@gmail.com>
2025-11-10 13:49:50 +0800

    feat(router): 建立 ProtectedRoute 並實作巢狀路由守衛

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import HomePage from './pages/HomePage'
+import ProtectedRoute from './components/layout/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
